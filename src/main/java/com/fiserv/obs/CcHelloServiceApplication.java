53e6e8c484c3f58dc5c7cefec5bfec1494d7fea2
Joachim Pasquali <joachim.pasquali@fiserv.com>
2024-06-09 13:06:05 -0400

    initial commit

---
+++ src/main/java/com/fiserv/obs/CcHelloServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fiserv.obs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcHelloServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcHelloServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiserv/obs/CcHelloServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiserv.obs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcHelloServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
